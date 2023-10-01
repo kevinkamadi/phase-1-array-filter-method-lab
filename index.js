c3ac4a8a10783b622c3d3a622e2b0ae81fc47867
Your Name <kkamadi567@gmail.com>
2023-10-01 23:00:07 +0300

    added

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+function findMatching(array, drivers){
+    const driversMatching = array.filter(function(element){
+        element.drivers===drivers
+        if(element === drivers){
+            return element
+        }else if(element.toLowerCase()===drivers.toLowerCase()){
+            return element
+        }
+
+    })
+    return driversMatching
+}
+
+function fuzzyMatch(array,letters){
+    const matchingLetters = array.filter((element)=> element.startsWith(letters) )
+    return matchingLetters
+}
+
+function matchName(array,drivers){
+    const nameMatches = array.filter((element) => element.name === drivers)
+    return nameMatches
+}
